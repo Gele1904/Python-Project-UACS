e8c1aa603f413f044401ce37c519215c1a56ff96
Gele1904 <angelnineski@gmail.com>
2023-05-25 15:25:01 +0200

    Update GameHandler added new game

--- GameHandler.py
+++ GameHandler.py
@@ -1,5 +1,7 @@
 import HangmanGame
 from TicTacToe import TicTacToe
+import RockPaperScissors
+import Instructions
 
 
 def playAgain():
@@ -14,22 +16,34 @@ def playAgain():
 
 
 while True:
-    print("-" * 45)
     print("\033[0mWelcome to our game menu, choose your game!")
+    print("-" * 45)
     print("1. Tic Tac Toe")
     print("2. Hangman")
     print("3. Rock Paper Scissors")
-    print("4. Exit")
-    option = input("Choose which game you want to play (1 - 3) (4 For Exit): ")
-    if option == "1":
+    print("\033[31m\033[1m0. Exit\033[0m")
+    print("-" * 45)
+    print("Instructions")
+    print("4. Tic Tac Toe Instructions")
+    print("5. Hangman Instructions")
+    print("6. Rock Paper Scissors Instructions")
+    print(" ")
+    option = int(input("Choose which game you want to play (1 - 3) (4 For Exit): "))
+    if option == 1:
         print("-" * 45)
-        TicTacToe.startGame()
-    elif option == "2":
+        TicTacToe.TicTacToe()
+    elif option == 2:
         print("-" * 45)
-        HangmanGame.hangMan()
-    elif option == "3":
-        print("The game is still in development!")
-    elif option == "4":
+        HangmanGame.HangMan()
+    elif option == 3:
+        RockPaperScissors.rock_paper_scissors()
+    elif option == 4:
+        Instructions.TicTacToeInstructions()
+    elif option == 5:
+        Instructions.HangManInstructions()
+    elif option == 6:
+        Instructions.RockPaperScissorsInstructions()
+    elif option == 0:
         break
     else:
         print("You chose a wrong number!")
