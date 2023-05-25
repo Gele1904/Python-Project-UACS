e836e31fbaeb89e0f425946028c99b45a74815be
Gele1904 <angelnineski@gmail.com>
2023-05-25 15:25:17 +0200

    Update - Added instructions for games

---
+++ Instructions.py
@@ -0,0 +1,55 @@
+def TicTacToeInstructions():
+    print("\033[36m---------------------------------------------\n"
+          "How to play Tic Tac Toe:\nTic Tac Toe is a game on a 3x3 grid, "
+          "two players take turns marking X or O in empty cells.\n"
+          "The goal is to form a line of three marks horizontally, vertically or diagonally.\n"
+          "---------------------------------------------\n"
+          "Advanced Tic Tac Toe Instructions:\n"
+          "-Start in the center for more opportunities.\n"
+          "- Look for winning patterns: rows,columns and diagonals.\n"
+          "- Create forks to force your opponent into difficult choices.\n"
+          "- Block your opponent's potential winning lines.\n"
+          "- Plan ahead and anticipate your opponent's moves.\n"
+          "- Use a defensive strategy when necessary.\n"
+          "- Aim for a draw if winning seems unlikely.\n"
+          "- Practice and analyze your games for improvement\n"
+          "Remember these tips to enhance your gameplay and enjoy the game!")
+
+
+def HangManInstructions():
+    print("\033[36m---------------------------------------------\n"
+          "How to play Hangman:\nHangman is a word-guessing game where players uncover a secret word "
+          "before a stick figure is formed."
+          " Take turns guessing letters until the word is revealed.\n"
+          "Keep playing until the word is guessed or the stick figure is completed\n"
+          "---------------------------------------------\n"
+          "Advanced Hangman Instructions:\n"
+          "- Choose more often vowels.\n"
+          "- Systematically guess high-frequency letters.\n"
+          "- Analyze revealed and missing letter patterns.\n"
+          "- Consider word categories or themes.\n"
+          "- Think creatively and guess unconventional letters.\n"
+          "- Keep track of guessed and revealed letters.\n"
+          "- Use context clues if available.\n"
+          "Remember these tips to enhance your gameplay and enjoy the game!")
+
+
+def RockPaperScissorsInstructions():
+    print("\033[36m---------------------------------------------\n"
+          "How to play Rock Paper Scissors:\nIn the game rock-paper-scissors two players make "
+          "simultaneous choices from three hand signs: rock, paper and scissors.\n"
+          "---------------------------------------------\n"
+          "The following rules dictate the outcomes:\n"
+          "Rock defeats scissors (as rock crushes scissors)\n"
+          "Scissors defeats paper (as scissors cuts paper)\n"
+          "Paper defeats rock (as paper wraps around rock)\n"
+          "---------------------------------------------\n"
+          "- Advanced Rock Paper Scissors Instructions.\n"
+          "- Vary your choices and avoid patterns.\n"
+          "- Pay attention to any patterns in the opponent's moves.\n"
+          "- Use psychology to simulate human behaviour.\n"
+          "- Predict based on previous outcomes and statistical trends.\n"
+          "- Counter common strategies used by players.\n"
+          "- Adapt to changing patterns during the game\n"
+          "- Stay focused and avoid biases.\n"
+          "Remember these tips to enhance your gameplay and enjoy the game!")
